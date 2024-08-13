d4634a6705e0fbf72754083fbaee1bd79823eac2
EstherAuma <estheraumaego@gmail.com>
2024-08-13 08:22:53 +0300

    Add validation to worker form

--- materialTracker/models.py
+++ materialTracker/models.py
@@ -7,8 +7,8 @@ from django.utils import timezone
 class Worker(models.Model):
     first_name = models.CharField(max_length=50, null=False, blank=False)
     last_name = models.CharField(max_length=50, null=False, blank=False)
-    phone_number = models.CharField(max_length=20, null=False, blank=False)
-    email = models.EmailField(max_length=50, null=False, blank=False)
+    phone_number = models.CharField(max_length=20, null=False, blank=False,unique=True)
+    email = models.EmailField(max_length=50, null=False, blank=False,unique=True)
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
