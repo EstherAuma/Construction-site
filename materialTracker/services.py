f951775bd3fb835c0ce210b0de1b43881ef362d6
EstherAuma <estheraumaego@gmail.com>
2024-10-09 15:58:30 +0300

    Aggregate form_questions

--- materialTracker/services.py
+++ materialTracker/services.py
@@ -10,66 +10,74 @@ class StrapiAPI:
             'Authorization': f'Bearer {self.access_token}'
         }
 
-    def camel_case(self, snake_str):
-        """Convert snake_case string to camelCase."""
-        components = snake_str.split('_')
-        return components[0] + ''.join(x.title() for x in components[1:])
-
     def get_content(self):
         try:
             response = requests.get(self.url, headers=self.headers)
             response.raise_for_status()
             data = response.json()
             content = data.get('data', [])
-
             formatted_data = []
+            locale_content = {}  
 
             for item in content:
                 custom_type = item.get('custom_type')
-                locale = item.get('locale')  
+                locale = item.get('locale')
                 print(f"Processing {custom_type}")
+                
+                locale_content.setdefault(locale, {})
 
                 if custom_type == "form_questions":
                     content_item = {
-                        "id": item.get("id"),
-                        "theme": item.get("theme"),
-                        "locale": locale,
                         "pageId": item.get("page_id"),
-                        "createdAt": item.get("createdAt"),
-                        "updatedAt": item.get("updatedAt"),
                         "pageTitle": item.get("page_title"),
-                        "publishedAt": item.get("published_at"),
+                        "lastUpdated": item.get("updatedAt"),
+                        
                         "customType": custom_type,
-                        "dynamicZone": {}
+                        "pageContent": {
+                            "list": []
+                        }
                     }
 
                     for component in item.get("dynamic_zone", []):
                         component_data = {
-                            "Key": component.get("Key"),
-                            "Label": component.get("Label"),
-                            "Options": component.get("Options"),
-                            "Category": component.get("Category"),
-                            "HelpText": component.get("HelpText"),
-                            "Required": component.get("Required"),
-                            "Placeholder": component.get("Placeholder"),
-                            "__component": component.get("__component"),
-                            "QuestionType": component.get("QuestionType"),
-                            "DisplayQuestion": component.get("DisplayQuestion"),
-                            "OptionTranslations": component.get("OptionTranslations"),
-                            "repeatableZoneItems": {}  
+                            "key": component.get("Key"),
+                            "label": component.get("Label"),
+                            "options": component.get("Options"),
+                            "category": component.get("Category"),
+                            "helpText": component.get("HelpText"),
+                            "required": component.get("Required"),
+                            "placeholder": component.get("Placeholder"),
+                            "component": component.get("__component"),
+                            "questionType": component.get("QuestionType"),
+                            "displayQuestion": component.get("DisplayQuestion"),
+                            "optionTranslations": component.get("OptionTranslations"),
                         }
+
+                        content_item["pageContent"]["list"].append(component_data)
 
                         if "repeatable_zone" in component:
                             for repeatable_item in component["repeatable_zone"]:
                                 repeatable_data = {
                                     "key": repeatable_item.get('key'),
-                                    "value": repeatable_item.get('value')
+                                    "value": repeatable_item.get('value'),
+                                    "id": repeatable_item.get("id")
                                 }
-                                component_data["repeatableZoneItems"][repeatable_item.get("id")] = repeatable_data
+                                content_item["pageContent"]["list"].append(repeatable_data)
 
-                        content_item["dynamicZone"][component.get("id")] = component_data
+                    locale_content[locale][item.get("page_id")] = content_item
 
-                    formatted_data.append(content_item)
+            for item in content:
+                formatted_data.append({
+                    "id": item.get("id"),
+                    "deleted": False,
+                    "createdAt": item.get("createdAt"),
+                    "updatedAt": item.get("updatedAt"),
+                    "deletedAt": item.get("deletedAt"),
+                    "createdBy": item.get("createdBy"),
+                    "updatedBy": item.get("updatedBy"),
+                    "deletedBy": item.get("deletedBy"),
+                    "content": locale_content
+                })
 
             self.save_to_database(formatted_data)
             return formatted_data
@@ -79,9 +87,10 @@ class StrapiAPI:
             return None
 
     def save_to_database(self, aggregated_data):
-        for content_item in aggregated_data:
-            
-            AggregatedContent.objects.create(
-                data=content_item  
-            )
+        for data_item in aggregated_data:
+            for locale, content in data_item["content"].items():
+                for content_item in content.values():
+                    AggregatedContent.objects.create(
+                        data=content_item
+                    )
         print("Data saved to the database.")
